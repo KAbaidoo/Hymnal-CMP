61c8bed3a913e2e6b51c747fee3bec3299845dc6
Kelvin Abaidoo <kabaidoo@hotmail.com>
2025-04-07 01:27:48 +0000

    Add AMDetailScreenContent

--- composeApp/src/androidMain/kotlin/com/kobby/hymnal/ancient_modern/AMDetailScreenContent.kt
+++ composeApp/src/androidMain/kotlin/com/kobby/hymnal/ancient_modern/AMDetailScreenContent.kt
@@ -1,31 +1,8 @@
 package com.kobby.hymnal.ancient_modern
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.kobby.hymnal.R
-import com.kobby.hymnal.components.DetailScreen
-import com.kobby.hymnal.core.components.ContentScreen
-import com.kobby.hymnal.core.components.SearchTextField
+import com.kobby.hymnal.core.components.DetailScreen
 import com.kobby.hymnal.theme.HymnalAppTheme
 
 
