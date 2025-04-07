cb04202298fb4fcc2c9bde204bf837d3205fa91e
Kelvin Abaidoo <kabaidoo@hotmail.com>
2025-04-07 01:27:13 +0000

    Add HighlightsScreen

---
+++ composeApp/src/androidMain/kotlin/com/kobby/hymnal/settings/Highlights.kt
@@ -0,0 +1,26 @@
+package com.kobby.hymnal.settings
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.kobby.hymnal.core.components.SettingsListScreen
+import com.kobby.hymnal.theme.HymnalAppTheme
+
+
+@Composable
+fun HighlightsScreenContent(){
+    val items = List(20) { _ -> "A&M HYMN 1" }
+    SettingsListScreen(
+        titleCollapsed = "Highlights",
+        items = items,
+        action = {
+        })
+}
+
+
+@Preview
+@Composable
+fun HighlightsScreenContentPreview() {
+    HymnalAppTheme {
+        HighlightsScreenContent()
+    }
+}
