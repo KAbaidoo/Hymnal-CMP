a366386ea9fefcbdea18eae7c5c7052d3d0fa708
Kelvin Abaidoo <kabaidoo@hotmail.com>
2025-04-07 01:28:29 +0000

    Add AllHymnsScreen

--- composeApp/src/androidMain/kotlin/com/kobby/hymnal/all_hymns/AllHymnsScreen.kt
+++ composeApp/src/androidMain/kotlin/com/kobby/hymnal/all_hymns/AllHymnsScreen.kt
@@ -2,8 +2,7 @@ package com.kobby.hymnal.all_hymns
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.kobby.hymnal.ancient_modern.AMDetailScreenContent
-import com.kobby.hymnal.components.ListScreen
+import com.kobby.hymnal.core.components.ListScreen
 import com.kobby.hymnal.theme.HymnalAppTheme
 
 @Composable
