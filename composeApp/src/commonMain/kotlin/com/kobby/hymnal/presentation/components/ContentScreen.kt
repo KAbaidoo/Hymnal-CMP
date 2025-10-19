604ed01c449bef5097082fd59133d36209716277
Kelvin Abaidoo <kabaidoo@hotmail.com>
2025-10-19 14:11:37 +0000

    Fix keyboard overlap issue in ContentScreen
    Apply `imePadding()` to the `Scaffold` in `ContentScreen` to prevent the on-screen keyboard from overlapping the UI, particularly the bottom bar or other content when it appears.

--- composeApp/src/commonMain/kotlin/com/kobby/hymnal/presentation/components/ContentScreen.kt
+++ composeApp/src/commonMain/kotlin/com/kobby/hymnal/presentation/components/ContentScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -131,7 +132,9 @@ fun ContentScreen(
         },
         bottomBar = bottomBar,
         containerColor = MaterialTheme.colorScheme.background,
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+        modifier = Modifier
+            .nestedScroll(scrollBehavior.nestedScrollConnection)
+            .imePadding()
     ) { innerPadding ->
         content(innerPadding)
     }
