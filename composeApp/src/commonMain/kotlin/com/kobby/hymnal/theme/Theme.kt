5794b186b6bbf74fe7a4588dfa9fae4551287e39
Kelvin Abaidoo <kabaidoo@hotmail.com>
2025-12-21 06:08:33 +0000

    feat(Theme): Change HymnalAppTheme from internal to public

--- composeApp/src/commonMain/kotlin/com/kobby/hymnal/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/kobby/hymnal/theme/Theme.kt
@@ -5,7 +5,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 
 @Composable
-internal fun HymnalAppTheme(
+fun HymnalAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ){
