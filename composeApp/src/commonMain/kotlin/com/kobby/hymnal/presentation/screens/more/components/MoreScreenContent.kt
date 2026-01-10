8383a463180805f8515672c734707bbf9bf6e2d9
Kelvin Abaidoo <kabaidoo@hotmail.com>
2026-01-10 11:44:11 +0000

    feat(paywall): Update MoreScreenContent to conditionally show support item and add preview function

--- composeApp/src/commonMain/kotlin/com/kobby/hymnal/presentation/screens/more/components/MoreScreenContent.kt
+++ composeApp/src/commonMain/kotlin/com/kobby/hymnal/presentation/screens/more/components/MoreScreenContent.kt
@@ -1,18 +1,14 @@
+@file:Suppress("UNUSED_IMPORT", "unused")
+
 package com.kobby.hymnal.presentation.screens.more.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -23,31 +19,32 @@ import com.kobby.hymnal.BuildKonfig
 import com.kobby.hymnal.presentation.components.ContentScreen
 import com.kobby.hymnal.presentation.components.ListItem
 import hymnal_cmp.composeapp.generated.resources.Res
-import hymnal_cmp.composeapp.generated.resources.settings
-import hymnal_cmp.composeapp.generated.resources.dark_mode
 import hymnal_cmp.composeapp.generated.resources.version
 import hymnal_cmp.composeapp.generated.resources.more
 import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MoreScreenContent(
-    isDarkMode: Boolean,
-    onDarkModeToggle: (Boolean) -> Unit,
     onItemClick: (String) -> Unit,
     onBackClick: () -> Unit,
-    onHomeClick: () -> Unit
+    onHomeClick: () -> Unit,
+    showSupportItem: Boolean = true
 ) {
     ContentScreen(
         titleCollapsed = stringResource(Res.string.more),
         titleExpanded = stringResource(Res.string.more),
         actionButtons = null,
         content = { innerPadding ->
-            val menuItems = listOf(
-                "Support Development",
+            val menuItems = mutableListOf<String>()
+            if (showSupportItem) {
+                menuItems.add("Support Development")
+            }
+            menuItems.addAll(listOf(
                 "Favorites",
                 "History",
                 "Highlights"
-            )
+            ))
 
             Column(
                 modifier = Modifier
@@ -78,5 +75,15 @@ fun MoreScreenContent(
         },
         onBackClick = onBackClick,
         onHomeClick = onHomeClick
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MoreScreenContentPreview() {
+    MoreScreenContent(
+        onItemClick = {},
+        onBackClick = {},
+        onHomeClick = {}
     )
 }
